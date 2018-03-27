c80922eb096c2cfbc602e199d3cd229315f13b60
Stephen M. Pallen <smpallen99@yahoo.com>
2018-03-27 00:14:44 -0400

    notification fixes WIP

--- app/renderer/js/notification/index.js
+++ app/renderer/js/notification/index.js
@@ -1,27 +1,37 @@
 'use strict';
 
 const {
-  remote: { app }
+	remote: { app }
 } = require('electron');
 
 const DefaultNotification = require('./default-notification');
-const { appId, loadBots } = require('./helpers');
+const { appId, setNotificationCallback } = require('./helpers');
 
 // From https://github.com/felixrieseberg/electron-windows-notifications#appusermodelid
 // On windows 8 we have to explicitly set the appUserModelId otherwise notification won't work.
 app.setAppUserModelId(appId);
 
-window.Notification = DefaultNotification;
+// window.Notification = DefaultNotification;
+let Notification = DefaultNotification;
 
 if (process.platform === 'darwin') {
 	const DarwinNotification = require('./darwin-notifications');
-	window.Notification = DarwinNotification;
+	//   window.Notification = DarwinNotification;
+	Notification = DarwinNotification;
 }
 
 window.addEventListener('load', () => {
 	// Call this function only when user is logged in
 	// eslint-disable-next-line no-undef, camelcase
-	if (page_params.realm_uri) {
-		loadBots();
-	}
+	setNotificationCallback((title, opts) => {
+		// const handlersOpts = opts.opts;
+		// delete opts.opts;
+		const localNotification = new Notification(title, opts);
+		// setHandlers(Notification, handlersOpts);
+		return localNotification;
+	});
+
+	// if (page_params.realm_uri) {
+	// 	// loadBots();
+	// }
 });
