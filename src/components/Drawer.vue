78304ab7539de2e4dfb05970a9c07a070256d1e7
Stephen Pallen <steve.pallen@emetrotel.com>
2020-07-11 16:30:47 -0400

    drawer tooltips and server shortcut labels

--- src/components/Drawer.vue
+++ src/components/Drawer.vue
@@ -13,43 +13,87 @@
       fill-height
     >
       <v-list dense>
-        <v-list-item
+        <template 
           v-for="(item, inx) in servers"
-          :key="inx"
-          :to="`/server/${item.serverId}`"
-          link
         >
-          <v-list-item-icon>
-            <v-img
-              :src="require('@/assets/icon-server.png')"
-              height="30"
-              width="30"
-            />
-          </v-list-item-icon>
-        </v-list-item>
-        <v-list-item
-          to="/organization/new"
+          <v-tooltip 
+            :key="inx"
+            right
+            :color="tipColor"
+          >
+            <template v-slot:activator="{ on, attrs }">
+              <v-list-item
+                :key="inx"
+                :to="`/server/${item.serverId}`"
+                link
+                v-bind="attrs"
+                v-on="on"
+              >
+                <v-list-item-icon 
+                  class="d-flex flex-column"
+                  style="height: 50px"
+                >
+                  <v-img
+                    :src="require('@/assets/icon-server.png')"
+                    height="30"
+                    width="30"
+                  />
+                  <span class="caption text-center mt-1">
+                    {{ `${userOSKey} ${inx}` }}
+                  </span>
+                </v-list-item-icon>
+              </v-list-item>
+            </template>
+            <span v-text="item.alias" />
+          </v-tooltip>
+        </template>
+        <v-tooltip 
+          right
+          :color="tipColor"
         >
-          <v-list-item-icon>
-            <v-icon>mdi-plus</v-icon>
-          </v-list-item-icon>
-        </v-list-item>
+          <template v-slot:activator="{ on, attrs }">
+            <v-list-item
+              to="/organization/new"
+              v-bind="attrs"
+              v-on="on"
+            >
+              <v-list-item-icon>
+                <v-icon>mdi-plus</v-icon>
+              </v-list-item-icon>
+            </v-list-item>
+          </template>
+          <span>Add Organization</span>
+        </v-tooltip>
       </v-list>
       <v-list 
         justify-end
         dense
       >
-        <v-list-item
+        <template
           v-for="item in items"
-          :key="item.title"
-          :to="item.to"
-          link
-          @click="click(item.id)"
         >
-          <v-list-item-icon>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-icon>
-        </v-list-item>
+          <v-tooltip
+            :key="item.title"
+            right
+            :color="tipColor"
+          >
+            <template v-slot:activator="{ on, attrs }">
+              <v-list-item
+                :key="item.title"
+                :to="item.to"
+                link
+                v-bind="attrs"
+                @click="click(item.id)"
+                v-on="on"
+              >
+                <v-list-item-icon>
+                  <v-icon>{{ item.icon }}</v-icon>
+                </v-list-item-icon>
+              </v-list-item>
+            </template>
+            <span v-text="item.title" />
+          </v-tooltip>
+        </template>
       </v-list>
     </v-layout>
   </v-navigation-drawer>
@@ -58,14 +102,20 @@
   import { get } from "vuex-pathify"
   import SystemUtil from '@/utils/system-util'
 
+  const mac = process.platform === 'darwin'
+  const userOSKey = mac ? '⌘' : 'Ctrl'
+
   export default {
     name: 'Drawer',
 
     data: () => ({
       items: [
         { icon: 'mdi-reload', title: 'Reload', to: '', id: 'reload' },
+        { icon: 'mdi-arrow-left', title: 'Go Back', to: '', id: 'back' },
         { icon: 'mdi-settings', title: 'Settings', to: '/settings' },
       ],
+      userOSKey: userOSKey,
+      tipColor: '#222',
     }),
 
     computed: {
